e464dad5a311018e1db8fa068b534231c5b6da4b
benryan1790 <37295698+benryan1790@users.noreply.github.com>
2026-02-10 17:12:40 +0000

    MTDSA-32974: add api code

---
+++ app/v1/list/ListPartnerIncomeConnector.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.list
+
+import api.config.AppConfig
+import api.connectors.DownstreamUri.HipUri
+import api.connectors.httpparsers.StandardDownstreamHttpParser.*
+import api.connectors.{BaseDownstreamConnector, DownstreamOutcome}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.http.client.HttpClientV2
+import v1.list.model.request.ListPartnerIncomeRequestData
+import v1.list.model.response.ListPartnerIncomeResponse
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class ListPartnerIncomeConnector @Inject() (val http: HttpClientV2, val appConfig: AppConfig) extends BaseDownstreamConnector {
+
+  def listPartnerIncome(request: ListPartnerIncomeRequestData)(implicit
+      hc: HeaderCarrier,
+      ec: ExecutionContext,
+      correlationId: String
+  ): Future[DownstreamOutcome[ListPartnerIncomeResponse]] = {
+
+    import request.*
+
+    get(HipUri[ListPartnerIncomeResponse](s"/itsa/income-tax/v1/${taxYear.asTysDownstream}/income/partnerships/$nino/list"))
+  }
+
+}
